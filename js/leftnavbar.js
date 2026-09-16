0b11d0d3a414d1ef4c76ea53ab9dff30ed70faeb
Pete <125869112+PeterJ34@users.noreply.github.com>
2026-09-16 11:16:11 +0100

    Change file names and code to referenece to leftnavbar

--- js/leftnavbar.js
+++ js/leftnavbar.js
@@ -1,4 +1,4 @@
- fetch("rightnavbar.html")
+ fetch("leftnavbar.html")
    .then((response) => response.text())
    .then((data) => {
      document.getElementById("nav-placeholder").innerHTML = data;
--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,4 @@
- fetch("rightnavbar.html")
+ fetch("navbar.html")
    .then((response) => response.text())
    .then((data) => {
      document.getElementById("nav-placeholder").innerHTML = data;
