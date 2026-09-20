0ce7fad098c844988fa00a8de73686553533858c
Pete <125869112+PeterJ34@users.noreply.github.com>
2026-09-20 12:50:11 +0100

    Modal added to pjnotes, Modal js added to leftnavbar.js

--- js/leftnavbar.js
+++ js/leftnavbar.js
@@ -1,8 +1,14 @@
- fetch("leftnavbar.html")
-   .then((response) => response.text())
-   .then((data) => {
-     document.getElementById("nav-placeholder").innerHTML = data;
-   })
-   .catch((error) => {
-     console.error("Error loading the navigation:", error);
-   });
+fetch("leftnavbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("nav-placeholder").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("Error loading the navigation:", error);
+  });
+
+const testModal = new bootstrap.Modal("#testModal");
+
+window.addEventListener("DOMContentLoaded", () => {
+  testModal.show();
+});
