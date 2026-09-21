4781a7d4f78acb960360533dcbfe3e86d9ce6cda
Pete <125869112+PeterJ34@users.noreply.github.com>
2026-09-21 09:36:04 +0100

    Modal on home page

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,39 @@
- fetch("navbar.html")
-   .then((response) => response.text())
-   .then((data) => {
-     document.getElementById("nav-placeholder").innerHTML = data;
-   })
-   .catch((error) => {
-     console.error("Error loading the navigation:", error);
-   });
+fetch("navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("nav-placeholder").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("Error loading the navigation:", error);
+  });
+
+const testModal = new bootstrap.Modal("#testModal");
+
+window.addEventListener("DOMContentLoaded", () => {
+  testModal.show();
+});
+document.getElementById("saveResponse").addEventListener("click", function () {
+  const name = document.getElementById("userName").value;
+  const menuChoice = document.getElementById("userRole").value;
+
+  const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLScWYxeyTjufh9Tgf7jZGTDYlFA9m7xMcqgkTWkheNDPSYxolg/formResponse";
+
+  const formData = new FormData();
+
+  formData.append("entry.1247776806", name);
+  formData.append("entry.2116715699", menuChoice);
+
+  fetch(formUrl, {
+    method: "POST",
+    mode: "no-cors",
+    body: formData,
+  });
+
+  // Close the Bootstrap modal
+  const modalElement = document.getElementById("testModal");
+  const modal = bootstrap.Modal.getInstance(modalElement);
+
+  if (modal) {
+    modal.hide();
+  }
+});
